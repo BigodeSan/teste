821d403ff2c573a9f63a4f0c554c6a9c18deadf7
BigodeSan <40863800+BigodeSan@users.noreply.github.com>
2019-02-11 12:46:28 -0200

    update

--- js/cep.js
+++ js/cep.js
@@ -1,4 +1,4 @@
-(function() {
+$(document).ready(function() {
     $('input[name="cep"]').blur(function(e) {
 
     var cep = $('input[name="cep"]').val()  || ''
@@ -6,20 +6,20 @@
     if (!cep) {
         return
     }
-var url = 'http://viacep.com.br/ws/' + cep + '/json' 
+    var url = 'http://viacep.com.br/ws/' + cep + '/json' 
 
-$.getJSON(url, function(data) {
-        if ("error" in data) {
-            return
-        } 
-        
-        $('input[name="rua"]').val(data.logradouro)
-        $('input[name="bairro"]').val(data.bairro)
-        $('input[name="cidade"]').val(data.localidade)
-        $('input[name="uf"]').val(data.uf)
+    $.getJSON(url, function(data) {
+            if ("error" in data) {
+                return
+            } 
+            
+            $('input[name="rua"]').val(data.logradouro)
+            $('input[name="bairro"]').val(data.bairro)
+            $('input[name="cidade"]').val(data.localidade)
+            $('input[name="uf"]').val(data.uf)
+
+            })
 
         })
-
-    })
 }) 
 
